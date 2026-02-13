b11f15977160c05053b3b93bca12a6eb1bb2ad52
Pavel Shvets <pavel.v.shvets@gmail.com>
2026-02-13 14:18:33 -0700

    added urls, views, sort and filter

--- e_commerce_data/urls.py
+++ e_commerce_data/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.e_commerce_data, name='e_commerce_data'),
+    path('e_commerce_data_describe/', views.e_commerce_data_describe, name='e_commerce_data_describe'),
+    path('e_commerce_data_info/', views.e_commerce_data_info, name='e_commerce_data_info'),
 ]
--- e_commerce_data/views.py
+++ e_commerce_data/views.py
@@ -1,11 +1,53 @@
 from django.shortcuts import render
 import pandas as pd
+import io
+
+pd.set_option("display.precision", 0)
+
+df = pd.read_csv('./e_commerce_data/Ecommerce_Sales_Data_2024_2025.csv')
 
 # Create your views here.
 def e_commerce_data(request):
+
     df = pd.read_csv('./e_commerce_data/Ecommerce_Sales_Data_2024_2025.csv')
 
+    columnSort = request.GET.get("columnSort", "")
+    ascending = request.GET.get("ascending", "")
+    columnFilter = request.GET.getlist("columnFilter", "")
+
+    if (columnSort or ascending or columnFilter):
+
+        ascending = True if ascending == "True" else False
+
+        if(columnSort):
+            df = df.sort_values(by=columnSort, ascending=ascending)
+
+        if(columnFilter):
+            df = df[columnFilter]
+
+
+
     table = df.to_html(classes='table table-striped', index=False)
+
     return render(request, 'ecommerce_data.html', {
         'table': table
+    })
+
+def e_commerce_data_describe(request):
+
+    df_description = df.describe()
+
+    return render(request, 'ecommerce_data_description.html', {
+        'df_description': df_description
+    })
+
+def e_commerce_data_info(request):
+
+    buffer = io.StringIO()
+    df.info(buf=buffer)
+    info_output = buffer.getvalue()
+
+    #table = df.to_html(classes='table table-striped', index=False)
+    return render(request, 'ecommerce_data_info.html', {
+        'data_info': info_output
     })
