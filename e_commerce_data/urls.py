51417dd05d7aaf8cf6cfc71e2ea0d8f14c7a2d1b
Pavel Shvets <pavel.v.shvets@gmail.com>
2026-02-14 13:46:47 -0700

    added checkboxes instead of select, added logic to process input from checkboxes, added a plot page to show plots and a function in the view to accept input form plots form and to show a plot

--- e_commerce_data/urls.py
+++ e_commerce_data/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.e_commerce_data, name='e_commerce_data'),
     path('e_commerce_data_describe/', views.e_commerce_data_describe, name='e_commerce_data_describe'),
     path('e_commerce_data_info/', views.e_commerce_data_info, name='e_commerce_data_info'),
+    path('plots/', views.plots, name='plots'),
 ]
--- e_commerce_data/views.py
+++ e_commerce_data/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import pandas as pd
+import plotly.express as px
 import io
 
 pd.set_option("display.precision", 0)
@@ -13,9 +14,56 @@ def e_commerce_data(request):
 
     columnSort = request.GET.get("columnSort", "")
     ascending = request.GET.get("ascending", "")
-    columnFilter = request.GET.getlist("columnFilter", "")
+    #columnFilter = request.GET.getlist("columnFilter", "")
 
-    if (columnSort or ascending or columnFilter):
+    orderID = request.GET.get("Order ID", "")
+    orderDate = request.GET.get("Order Date", "")
+    customerName = request.GET.get("Customer Name", "")
+    region = request.GET.get("Region", "")
+    city = request.GET.get("City", "")
+    category = request.GET.get("Category", "")
+    subCategory = request.GET.get("Sub-Category", "")
+    productName = request.GET.get("Product Name", "")
+    quantity = request.GET.get("Quantity", "")
+    unitPrice = request.GET.get("Unit Price", "")
+    discount = request.GET.get("Discount", "")
+    sales = request.GET.get("Sales", "")
+    profit = request.GET.get("Profit", "")
+    paymentMode = request.GET.get("Payment Mode", "")
+    columnFilter = []
+
+    if (orderID or orderDate or customerName or region or city or category or subCategory or productName or quantity or unitPrice or discount or sales or profit or paymentMode):
+        if orderID:
+           columnFilter.append(orderID) 
+        if orderDate:
+            columnFilter.append(orderDate)
+        if customerName:
+            columnFilter.append(customerName)
+        if region:
+            columnFilter.append(region)
+        if city:
+            columnFilter.append(city)
+        if category:
+            columnFilter.append(category)
+        if subCategory:
+            columnFilter.append(subCategory)
+        if productName:
+            columnFilter.append(productName)
+        if quantity:
+            columnFilter.append(quantity)
+        if unitPrice:
+            columnFilter.append(unitPrice)
+        if discount:
+            columnFilter.append(discount)
+        if sales:
+            columnFilter.append(sales)
+        if profit:
+            columnFilter.append(profit)
+        if paymentMode:
+            columnFilter.append(paymentMode)
+
+
+    if (columnSort or ascending or (len(columnFilter) > 0)):
 
         ascending = True if ascending == "True" else False
 
@@ -24,7 +72,6 @@ def e_commerce_data(request):
 
         if(columnFilter):
             df = df[columnFilter]
-
 
 
     table = df.to_html(classes='table table-striped', index=False)
@@ -50,4 +97,38 @@ def e_commerce_data_info(request):
     #table = df.to_html(classes='table table-striped', index=False)
     return render(request, 'ecommerce_data_info.html', {
         'data_info': info_output
+    })
+
+def plots(request):
+    salesByProduct = request.GET.get("salesByProduct", "")
+
+    graph = px.bar(
+        df.groupby("Product Name")["Sales"].sum().reset_index(),
+        x="Product Name",
+        y="Sales",
+        title="Total Sales by Product"
+        #color="Product Name",
+        #color_discrete_sequence=["black"]
+    )
+
+    #graph.update_traces(marker_color="black")
+
+    graph.update_traces(marker_color="black")
+
+    #graph.update_layout(template="simple_white")
+
+    """
+    graph.update_layout(
+        plot_bgcolor="white",
+        paper_bgcolor="white",
+        font=dict(color="black"),
+        xaxis=dict(showgrid=False),
+        yaxis=dict(showgrid=True, gridcolor="gray")
+    )
+    """
+
+    graph_html = graph.to_html(full_html=False)
+
+    return render(request, 'plots.html', {
+        'plot': graph_html
     })
